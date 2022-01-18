d86df637146c070e2622bb7accb26793a4682881
李涵 <2206359598@qq.com>
2022-01-18 17:13:12 +0000

    Add files via upload

--- src/test/scala/components/RegFileTest.scala
+++ src/test/scala/components/RegFileTest.scala
@@ -5,6 +5,22 @@ import chiseltest._
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
-class IfIdStageTest extends AnyFreeSpec with ChiselScalatestTester with Matchers {
-  
+class RegFileTest extends AnyFreeSpec with ChiselScalatestTester with Matchers {
+  "Register File Test" in {
+    test(new RegFile()) { s =>
+      s.io.writeEnable.poke(1.B)
+      s.io.writeData.poke(1037.U(32.W))
+      s.io.writeAddr.poke(0.U(5.W))
+      s.io.r1Addr.poke(0.U(5.W))
+      s.clock.step()
+      s.io.r1Data.expect(0.U(32.W))
+      ////
+      s.io.writeEnable.poke(1.B)
+      s.io.writeData.poke(1037.U(32.W))
+      s.io.writeAddr.poke(3.U(5.W))
+      s.io.r1Addr.poke(3.U(5.W))
+      s.clock.step()
+      s.io.r1Data.expect(1037.U(32.W))
+    }
+  }
 }
