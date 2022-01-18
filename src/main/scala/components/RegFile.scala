7c950b634932a6ef57afa6d655c2d1fe5188e300
Banson Xu <xzcxzcyy@126.com>
2022-01-18 17:50:08 +0800

    add regfile; missing its test.

---
+++ src/main/scala/components/RegFile.scala
@@ -0,0 +1,30 @@
+package components
+
+import chisel3._
+
+class Regfile extends Module {
+  val io = IO(new RegFileIO)
+  val regfile = RegInit(VecInit(Seq.fill(config.numOfReg)(0.U(config.regDataWidth))))
+  io.r1Data := regfile(io.r1Addr)
+  io.r2Data := regfile(io.r2Addr)
+  when (io.writeEnable & (io.writeAddr =/= 0.U(config.regAddrWidth))) {
+    regfile(io.writeAddr) := io.writeData
+  }
+}
+
+private object config {
+  val regAddrWidth = 5.W
+  val regDataWidth = 32.W
+  val numOfReg = 32
+}
+
+class RegFileIO extends Bundle {
+  val r1Addr = Input(UInt(config.regAddrWidth))
+  val r2Addr = Input(UInt(config.regAddrWidth))
+  val writeAddr = Input(UInt(config.regAddrWidth))
+  val writeData = Input(UInt(config.regDataWidth))
+  val writeEnable = Input(Bool())
+  ///
+  val r1Data = Output(UInt(config.regDataWidth))
+  val r2Data = Output(UInt(config.regDataWidth))
+}
---
+++ src/test/scala/components/RegFileTest.scala
@@ -0,0 +1,10 @@
+package components
+
+import chisel3._
+import chiseltest._
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class IfIdStageTest extends AnyFreeSpec with ChiselScalatestTester with Matchers {
+  
+}
