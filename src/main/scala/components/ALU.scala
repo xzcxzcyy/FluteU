a59fac69ab5e46611a9cf3c43142497f0d2e2dfd
Banson Xu <xzcxzcyy@126.com>
2022-01-22 15:32:10 +0800

    feat: write ALU (require test)

--- src/main/scala/components/ALU.scala
+++ src/main/scala/components/ALU.scala
@@ -2,6 +2,7 @@ package components
 
 import chisel3._
 import config.CpuConfig._
+import chisel3.util.MuxLookup
 
 object ALUOp {
   // Bitwise Ops
@@ -10,13 +11,77 @@ object ALUOp {
   val xor = 3.U(aluOpWidth.W)
   val nor = 4.U(aluOpWidth.W)
   // Shift Ops
-  val sll = 5.U(aluOpWidth.W) // shift logic left
-  val slr = 6.U(aluOpWidth.W) // shift logic right
-  val sar = 7.U(aluOpWidth.W) // shift arithmetic right
+  /** Logical shift left: rd ← rt << shamt. Fills bits from right with zeros. Logical shift right:
+    * rd ← rt >> shamt. Fills bits from left with zeros. Arithmetic shift right: If rt is negative,
+    * the leading bits are filled in with ones instead of zeros: rd ← rt >> shamt.
+    *
+    * Mind the order of shift oprands.
+    */
+  val sll = 5.U(aluOpWidth.W) // shift left  logically
+  val srl = 6.U(aluOpWidth.W) // shift right logically
+  val sra = 7.U(aluOpWidth.W) // shift right arithmetically
   // Set Ops
   val slt  = 8.U(aluOpWidth.W) // set less than (signed)
   val sltu = 9.U(aluOpWidth.W) // set less than (unsigned)
   // Sub and Add Ops
-  val add = 10.U(aluOpWidth.W)
-  val sub = 11.U(aluOpWidth.W)
+  val add  = 10.U(aluOpWidth.W)
+  val sub  = 11.U(aluOpWidth.W)
+  val addu = 12.U(aluOpWidth.W)
+  val subu = 13.U(aluOpWidth.W)
+}
+
+class ALU extends Module {
+  class Flag extends Bundle {
+    val equal = Bool()
+    val lessU = Bool()
+    val lessS = Bool()
+    val trap  = Bool()
+  }
+
+  val io = IO(new Bundle {
+    val aluOp  = Input(UInt(aluOpWidth.W))
+    val x      = Input(UInt(dataWidth.W))
+    val y      = Input(UInt(dataWidth.W))
+    val result = Output(UInt(dataWidth.W))
+    val flag   = Output(new Flag())
+  })
+
+  io.flag.equal := io.x === io.y
+  io.flag.lessS := io.x.asSInt < io.y.asSInt
+  io.flag.lessU := io.x.asUInt < io.y.asUInt
+
+  val x      = io.x
+  val y      = io.y
+  val addRes = x + y
+  val subRes = x - y
+  val overflow = MuxLookup(
+    key = io.aluOp,
+    default = 0.B,
+    mapping = Seq(
+      ALUOp.add -> ((x(31) & y(31) & ~addRes(31)) | (~x(31) & ~y(31) & addRes(31))),
+      ALUOp.sub -> ((x(31) & ~y(31) & ~addRes(31)) | (~x(31) & y(31) & addRes(31))),
+    )
+  )
+
+  io.flag.trap := overflow
+
+  io.result := MuxLookup(
+    key = io.aluOp,
+    default = 0.U,
+    mapping = Seq(
+      ALUOp.and  -> (x & y),
+      ALUOp.or   -> (x | y),
+      ALUOp.xor  -> (x ^ y),
+      ALUOp.nor  -> ~(x | y),
+      ALUOp.sll  -> (y << x(4, 0)),
+      ALUOp.srl  -> (y >> x(4, 0)),
+      ALUOp.sra  -> (y.asSInt >> x(4, 0)).asUInt,
+      ALUOp.slt  -> (io.x.asSInt < io.y.asSInt).asUInt,
+      ALUOp.sltu -> (io.x.asUInt < io.y.asUInt).asUInt,
+      ALUOp.add  -> addRes,
+      ALUOp.sub  -> subRes,
+      ALUOp.addu -> addRes,
+      ALUOp.subu -> subRes,
+    )
+  )
 }
