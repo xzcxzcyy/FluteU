b80278e5e3dbd80bd8e7ce028e06f81dc09af56b
Banson Xu <xzcxzcyy@126.com>
2022-01-29 14:34:32 +0800

    feat: Write-back segment.

---
+++ src/main/scala/pipeline/WriteBack.scala
@@ -0,0 +1,17 @@
+package pipeline
+
+import chisel3._
+
+import config.CpuConfig._
+
+class WriteBack extends Module {
+  val io = IO(new Bundle {
+    val fromMem       = Input(new MemWbBundle)
+    val regWriteEn    = Output(Bool())
+    val writeBackData = Output(UInt(dataWidth.W))
+    val writeRegAddr  = Output(UInt(regAddrWidth.W))
+  })
+  io.regWriteEn    := io.fromMem.control.regWriteEn
+  io.writeBackData := Mux(io.fromMem.control.memToReg, io.fromMem.dataFromMem, io.fromMem.aluResult)
+  io.writeRegAddr  := io.fromMem.writeRegAddr
+}
