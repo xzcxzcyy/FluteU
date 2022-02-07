69d2a889a453040c05b4c0ad73ce192d8297a36e
Chengyuan-artist <1136427613@qq.com>
2022-02-07 13:09:29 +0800

    feat: MockInstrMem add ready interface

--- src/main/scala/mock/MockInstrMem.scala
+++ src/main/scala/mock/MockInstrMem.scala
@@ -2,20 +2,32 @@ package mock
 
 import chisel3._
 import chisel3.util.experimental.loadMemoryFromFileInline
+import config.CpuConfig._
 
 class MockInstrMem(memoryFile: String = "") extends Module {
   val width: Int = 32
   val io = IO(new Bundle {
-    val addr = Input(UInt(10.W))
+    val addr = Input(UInt(addrWidth.W))
     val dataOut = Output(UInt(width.W))
+    val ready = Output(Bool())
   })
 
   val mem = SyncReadMem(1024, UInt(width.W))
+  val addr = RegInit(0.U(width.W))
+
   // Initialize memory
   if (memoryFile.trim().nonEmpty) {
     loadMemoryFromFileInline(mem, memoryFile)
   }
-  io.dataOut := mem(io.addr >> 2)
+
+  val cut_addr = io.addr(31,2)
+
+  io.ready := (cut_addr === addr)
+  io.dataOut := mem(cut_addr)
+
+  addr := cut_addr
+
   printf("Input is %d\t", io.addr)
-  printf("Output is %x\n", io.dataOut)
+  printf("dataOut is %x\t", io.dataOut)
+  printf("ready is %d\n", io.ready)
 }
