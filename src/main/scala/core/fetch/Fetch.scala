df06975d896e06e39e109b5f4b4cffac6a8c7454
Jian Zhou <i@janzhou.org>
2022-02-26 12:11:48 +0800

    core: fetch io

--- src/main/scala/core/fetch/Fetch.scala
+++ src/main/scala/core/fetch/Fetch.scala
@@ -8,6 +8,9 @@ import flute.cache.ICacheIO
 import flute.core.execute.ExecuteFeedbackIO
 
 class FetchIO(implicit conf: CPUConfig) extends Bundle {
+    val insts       = Vec(8, SInt(32.W)) // 指令队列，不确定指令用什么格式往下传，需要附带什么信息，暂用32位表示
+    val fetched     = Output(UInt(3.W))  // 有效指令长度，不超过8
+    val processed   = Input(UInt(3.W))  // 被Decode取走数量
 }
 
 class FetchFeedback(implicit conf: CPUConfig) extends Bundle {
