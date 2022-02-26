d2361b65e2ec1183446f6bd0390d0a940c09cfdb
Chengyuan-artist <1136427613@qq.com>
2022-02-27 01:43:16 +0800

    test finish;But I dont konw if its behavior right

--- src/test/scala/cache/ICacheTest.scala
+++ src/test/scala/cache/ICacheTest.scala
@@ -8,10 +8,9 @@ import org.scalatest.matchers.should.Matchers
 import fluteutil.BitMode.fromIntToBitModeLong
 import config.CpuConfig._
 
-
 class ICacheTest extends AnyFreeSpec with ChiselScalatestTester with Matchers {
   "test" in {
-    test(new ICache) { c => 
+    test(new ICache) { c =>
       c.io.addr.bits.poke(4.U)
       c.io.addr.valid.poke(1.B)
 
@@ -19,7 +18,38 @@ class ICacheTest extends AnyFreeSpec with ChiselScalatestTester with Matchers {
 
       c.io.data.valid.expect(1.B)
 
-      printf(p"${c.io.data.bits}")
+      c.io.data.bits(0).expect(0x380200ff.BM.U)
+      for (i <- 1 to 7) {
+        if (i == 4) {
+          c.io.data.bits(i).expect(0x00221826.BM.U)
+        } else {
+          c.io.data.bits(i).expect(0.U)
+        }
+      }
+
+      c.io.addr.valid.poke(0.B)
+      c.clock.step()
+      c.io.data.valid.expect(1.B)
+      c.clock.step()
+      c.io.data.valid.expect(0.B)
+
+
+      c.io.addr.bits.poke(4.U)
+      c.io.addr.valid.poke(1.B)
+
+      c.clock.step(5)
+
+      c.io.data.valid.expect(1.B)
+
+
+      c.io.addr.bits.poke(0.U)
+      c.clock.step()
+      c.io.data.valid.expect(1.B)
+      c.io.data.bits(0).expect(0x380200ff.BM.U)
+      c.clock.step()
+      c.io.data.valid.expect(1.B)
+      c.io.data.bits(0).expect(0x3801ffff.BM.U)
+
     }
   }
 }
