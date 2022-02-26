a8b0b290773f17df9ce1c3367cec859defde9397
Banson Xu <xzcxzcyy@126.com>
2022-02-27 00:12:30 +0800

    icache completed; requires tests

--- src/main/scala/cache/ICache.scala
+++ src/main/scala/cache/ICache.scala
@@ -4,14 +4,59 @@ import flute.config._
 
 import chisel3._
 import chisel3.util.MuxLookup
+import chisel3.util.DecoupledIO
 
 class ICacheIO(implicit conf: CPUConfig) extends Bundle {
-  val addr = Input(UInt(conf.addrWidth.W))
-  val data = Output(UInt(conf.dataWidth.W))
+  val addr = Flipped(new DecoupledIO(UInt(conf.addrWidth.W)))
+  val data = new DecoupledIO(Vec(conf.fetchGroupSize, UInt(conf.dataWidth.W)))
 }
 
 class ICache(implicit conf: CPUConfig) extends Module {
-  val io = IO(new Bundle {
-    val port = new ICacheIO()
-  })
+  val io = IO(new ICacheIO)
+
+  val memAddr = io.addr.bits(31, 2)
+  
+  val mem  = SyncReadMem(1024, UInt(conf.dataWidth.W))
+  val addrBuf = RegInit(0.U(conf.addrWidth.W))
+  val dataBuf = RegInit(VecInit(Seq.fill(conf.fetchGroupSize)(0.U(conf.instrWidth.W))))
+  val state = RegInit(0.U(2.W))
+
+  val memPorts = for (i <- 0 to conf.fetchGroupSize - 1) yield mem.read(memAddr + i.U)
+
+  when (state === 0.U) {
+    when (io.addr.valid) {
+      state := 1.U
+      addrBuf := io.addr.bits
+    } .otherwise {
+      state := 0.U
+    }
+  } .elsewhen(state === 1.U) {
+    for (i <- 0 to conf.fetchGroupSize - 1) yield dataBuf(i.U) := memPorts(i)
+    when (io.addr.valid) {
+      state := 2.U
+      addrBuf := io.addr.bits
+    } .otherwise {
+      state := 3.U
+    }
+  } .elsewhen(state === 2.U) {
+    for (i <- 0 to conf.fetchGroupSize - 1) yield dataBuf(i.U) := memPorts(i)
+    when (io.addr.valid) {
+      state := 2.U
+      addrBuf := io.addr.bits
+    } .otherwise {
+      state := 3.U
+    }
+  } .otherwise {
+    when (io.addr.valid) {
+      state := 1.U
+      addrBuf := io.addr.bits
+    } .otherwise {
+      state := 0.U
+    }
+
+  }
+
+  io.data.valid := (state === 2.U || state === 3.U)
+  io.addr.ready := 1.B
+  io.data.bits := dataBuf
 }
---
+++ src/test/scala/cache/ICacheTest.scala
@@ -0,0 +1,22 @@
+package cache
+
+
+import chisel3._
+import chisel3.experimental.VecLiterals._
+import chiseltest._
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+import fluteutil.BitMode.fromIntToBitModeLong
+import flute.config
+import flute.cache.ICache
+
+
+class ICacheTest extends AnyFreeSpec with ChiselScalatestTester with Matchers {
+  "TEST" in {
+    test() { c => 
+
+
+    }
+  }
+}
