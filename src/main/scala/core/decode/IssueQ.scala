12e1b724ae236dd19f55acfd9427230c49fd9c74
Chengyuan-artist <1136427613@qq.com>
2022-02-28 14:30:49 +0800

    issue queue finined;test needed

--- src/main/scala/core/decode/IssueQ.scala
+++ src/main/scala/core/decode/IssueQ.scala
@@ -33,21 +33,22 @@ class IssueQEntry extends Bundle {
   val ready = Bool()
 }
 
-class IssueQ extends Module {
+class IssueQ[T <: Data](entryType : T) extends Module {
   val io = IO(new Bundle {
-    val dataOut  = Output(Vec(16, new IssueQEntry))
+    val dataOut  = Output(Vec(16, entryType))
     val entryNum = Output(UInt(log2Up(16).W))
 
-    // enqueue
-    val readAddr = Input(Vec(2, ValidIO(UInt(log2Up(16).W))))
-    val readData = Input(Vec(2, ValidIO(new IssueQEntry)))
+    /// enqueue
+    val enqAddr = Input(Vec(2, ValidIO(UInt(log2Up(16).W))))
+    val enqData = Input(Vec(2, ValidIO(entryType)))
 
-    // issue
+    /// issue
     val issueAddr = Input(Vec(2, ValidIO(UInt(log2Up(16).W))))
-    val issueData = Output(Vec(2, ValidIO(new IssueQEntry)))
+    // provide that issueAddr(0) < issueAddr(1)
+    val issueData = Output(Vec(2, ValidIO(entryType)))
   })
 
-  val mem      = Mem(16, new IssueQEntry)
+  val mem      = Mem(16, entryType)
   val entryNum = RegInit(0.U(log2Up(16).W))
 
   object MoveState {
@@ -60,7 +61,7 @@ class IssueQ extends Module {
 
   val ctrl = Wire(Vec(16, UInt(2.W)))
 
-  for (i <- 0 until 16 - 2) {
+  for (i <- 0 until 16) {
     val data = mem.read(i.U)
     val pre1 = if (i > 15) data else mem.read((i + 1).U)
     val pre2 = if (i > 14) data else mem.read((i + 2).U)
@@ -74,8 +75,8 @@ class IssueQ extends Module {
           MoveState.stay       -> data,
           MoveState.preFirst   -> pre1,
           MoveState.preSecond  -> pre2,
-          MoveState.readFirst  -> io.readData(0).bits,
-          MoveState.readSecond -> io.readData(1).bits
+          MoveState.readFirst  -> io.enqData(0).bits,
+          MoveState.readSecond -> io.enqData(1).bits
         )
       )
     )
@@ -84,6 +85,28 @@ class IssueQ extends Module {
   for (i <- 0 until 2) {
     io.issueData(i).bits  := mem.read(io.issueAddr(i).bits)
     io.issueData(i).valid := io.issueAddr(i).valid
+  }
+
+  val numEnq = PopCount(io.enqAddr.map(_.valid))   // 下一拍入队数量，由分配单元保证合法
+  val numDeq = PopCount(io.issueAddr.map(_.valid)) // 下一拍出队数量，由仲裁单元保证合法
+
+  entryNum := entryNum + numEnq - numDeq
+  io.entryNum := entryNum
+
+  val issueAddr = Wire(Vec(2, UInt(log2Up(16).W)))
+  for (i <- 0 until 2) issueAddr(i) := Mux(io.issueAddr(i).valid, io.issueAddr(i).bits, 32.U)
+
+  for (i <- 0 until 16) {
+    ctrl(i) := MuxCase(
+      default = MoveState.stay,
+      mapping = Seq(
+        (io.enqAddr(0).valid && i.U === io.enqAddr(0).bits) -> MoveState.readFirst,
+        (io.enqAddr(1).valid && i.U === io.enqAddr(1).bits) -> MoveState.readSecond,
+        (i.U < issueAddr(0))                                -> MoveState.stay,
+        (i.U >= issueAddr(0) && i.U < issueAddr(1) - 1.U)   -> MoveState.preFirst,
+        (i.U >= issueAddr(1) - 1.U)                         -> MoveState.preSecond
+      )
+    )
   }
 
 }
