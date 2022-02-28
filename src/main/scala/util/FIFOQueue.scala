ec8a812abe501fc927c12409139da353d7069222
Banson Xu <xzcxzcyy@126.com>
2022-02-28 16:01:50 +0800

    fix: slight bug(s)

--- src/main/scala/util/FIFOQueue.scala
+++ src/main/scala/util/FIFOQueue.scala
@@ -6,67 +6,63 @@ import chisel3.util._
 class FIFOBundle[T <: Data](gen: T, numRead: Int, numWrite:Int) extends Bundle {
   val read  = Vec(numRead, Decoupled(gen))
   val write = Flipped(Vec(numWrite, Decoupled(gen)))
+  // val test  = Output(UInt(32.W))
 }
 
 class FIFOQueue[T <: Data](gen:T, numEntries: Int, numRead: Int, numWrite: Int) extends Module {
+  assert(isPow2(numEntries) && numEntries > 1)
+
   val io = IO(new FIFOBundle(gen, numRead, numWrite))
+
+  // io.test := 2.U(3.W) - 5.U(3.W)
 
   val data = Mem(numEntries, gen)
 
+  // tail是数据入队的位置（该位置目前没数据），head是数据出队的第一个位置（该位置放了最老的数据）
   val head_ptr = RegInit(0.U(log2Up(numEntries).W))
   val tail_ptr = RegInit(0.U(log2Up(numEntries).W))
 
-  val difference   = head_ptr - tail_ptr
-  val deqEntries = Mux(tail_ptr < head_ptr, difference, numEntries.U - difference)
-  val enqEntries = Mux(tail_ptr < head_ptr, difference, numEntries.U - difference)
+  // 为了区分空队列和满队列满，队列不允许真正全部放满（numEntries=8时，有8个位置，但同一时刻最多只能用7个）
+  val difference = tail_ptr - head_ptr
+  val deqEntries = difference
+  val enqEntries = (numEntries - 1).U - difference
 
   val numTryEnq = PopCount(io.write.map(_.valid))
   val numTryDeq = PopCount(io.read.map(_.ready))
-  val numDeq = Mux(deqEntries < numTryDeq, difference, numEntries.U - difference)
-  val numEnq = Mux(enqEntries < numTryEnq, difference, numEntries.U - difference)
+  val numDeq = Mux(deqEntries < numTryDeq, deqEntries, numTryDeq)
+  val numEnq = Mux(enqEntries < numTryEnq, enqEntries, numTryEnq)
 
+  // Assumptions:
+  // 读写端口的均需要把1集中放前面
+  // 类似[1, 0, 1]的读写行为是未定义的
   for (i <- 0 until numWrite) {
-    val offset = Wire(UInt(log2Up(numEntries).W))
-    if (i == 0) {
-      offset := 0.U
-    } else {
-      offset := PopCount(io.write.map(_.valid)(i-1))
-    }
+    val offset = i.U
 
     when (io.write(i).valid) {
       when (offset < numEnq) {
-        data(head_ptr + offset) := io.write(i).bits
-        io.write(i).ready := 1.B
-      }.otherwise{
-        io.write(i).ready := 0.B
+        data((tail_ptr + offset)(log2Up(numEntries) - 1, 0)) := io.write(i).bits
       }
-    }.otherwise{
-        io.write(i).ready := 0.B
     }
+
+    io.write(i).ready := offset < numEnq
   }
 
   for (i <- 0 until numRead) {
-    val offset = Wire(UInt(log2Up(numEntries).W))
-    if (i == 0) {
-      offset := 0.U
-    } else {
-      offset := PopCount(io.read.map(_.ready)(i-1))
-    }
+    val offset = i.U
 
     when (io.read(i).ready) {
       when (offset < numDeq) {
-        io.read(i).bits := data(tail_ptr + offset)
-        io.read(i).valid := 1.B
+        io.read(i).bits := data((head_ptr + offset)(log2Up(numEntries) - 1, 0))
       }.otherwise{
         io.read(i).bits := DontCare
-        io.read(i).valid := 0.B
       }
     }.otherwise{
       io.read(i).bits := DontCare
-      io.read(i).valid := 0.B
     }
+
+    io.read(i).valid := offset < numDeq
   }
 
-  head_ptr := head_ptr + numEnq
-  tail_ptr := tail_ptr + numDeq
+  head_ptr := head_ptr + numDeq
+  tail_ptr := tail_ptr + numEnq
 }
--- src/test/scala/util/FIFOTest.scala
+++ src/test/scala/util/FIFOTest.scala
@@ -7,19 +7,26 @@ import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 import fluteutil.BitMode.fromIntToBitModeLong
-import config.CpuConfig._
+import config.CPUConfig._
 
 class FIFOTest extends AnyFreeSpec with ChiselScalatestTester with Matchers {
-  "fifo test" in {
+  /**
+    * Should be further tested.
+    */
+  "general tests" in {
     test(new FIFOQueue(UInt(32.W), 1024, 2, 2)) { fifo =>
+      // fifo.io.test.expect(0.U(32.W))
       for (i <- 0 until 2) {
         fifo.io.write(i).bits.poke(i.U)
         fifo.io.write(i).valid.poke(1.B)
-        fifo.io.read(i).ready.poke(1.B)
+        fifo.io.read(i).valid.expect(0.B)
       }
       fifo.clock.step()
       for (i <- 0 until 2) {
+        fifo.io.read(i).ready.poke(1.B)
         fifo.io.write(i).ready.expect(1.B)
+        fifo.io.read(i).valid.expect(1.B)
+        fifo.io.read(i).bits.expect(i.U)
       }
     }
   }
