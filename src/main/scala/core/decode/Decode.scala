fc190d9c8b6536823e59a6f134614fbabf825a79
Chengyuan-artist <1136427613@qq.com>
2022-03-01 18:35:52 +0800

    connect fecth with decode

--- src/main/scala/core/decode/Decode.scala
+++ src/main/scala/core/decode/Decode.scala
@@ -24,11 +24,30 @@ class Decode extends Module {
 
   val regFile = Module(new RegFile(superscalar, superscalar))
 
+  val issueQueue = Module(new IdeaIssueQueue(new MicroOp))
+
   val decoders = for (i <- 0 until superscalar) yield Module(new Decoder)
+
+  val willProcess = Mux(io.withFetch.instNum < 2.U, io.withFetch.instNum, 2.U)
+  // willProcess = min(2, instNum)
+
   for (i <- 0 until superscalar) {
     // read
     regFile.io.read(i) <> decoders(i).io.withRegfile
     // write
-    regFile.io.write(i) := io.regFileWrite
+    regFile.io.write(i) := io.regFileWrite(i)
+
+    // fetch
+    decoders(i).io.instr := io.withFetch.insts(i)
+
+    // issue
+    issueQueue.io.in(i).bits  := decoders(i).io.microOp
+    issueQueue.io.in(i).valid := Mux(willProcess > i.U, 1.B, 0.B)
+
+    // ex
+    io.withExecute.microOps(i) <> issueQueue.io.out(i)
   }
+
+  io.withFetch.willProcess := willProcess
+
 }
