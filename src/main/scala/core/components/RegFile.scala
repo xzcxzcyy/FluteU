1f1ed999440aa36c6f96715240a7c9fb6d896705
Banson Xu <xzcxzcyy@126.com>
2022-03-01 11:29:52 +0800

    feat: multiple port regfile

--- src/main/scala/core/components/RegFile.scala
+++ src/main/scala/core/components/RegFile.scala
@@ -3,27 +3,38 @@ package core.components
 import chisel3._
 import config.CPUConfig._
 
-class RegFile extends Module {
-  val io = IO(new RegFileIO)
+class RegFile(numRead: Int, numWrite: Int) extends Module {
+  val io      = IO(new RegFileIO(numRead, numWrite))
   val regfile = RegInit(VecInit(Seq.fill(regAmount)(0.U(dataWidth.W))))
-  io.r1Data := regfile(io.r1Addr)
-  io.r2Data := regfile(io.r2Addr)
-  when (io.writeEnable && (io.writeAddr =/= 0.U(regAddrWidth.W))) {
-    regfile(io.writeAddr) := io.writeData
+  for (i <- 0 to numRead) {
+    io.read(i).r1Data := regfile(io.read(i).r1Addr)
+    io.read(i).r2Data := regfile(io.read(i).r2Addr)
   }
-
+  for (i <- 0 to numWrite) {
+    when(io.write(i).writeEnable && (io.write(i).writeAddr =/= 0.U(regAddrWidth.W))) {
+      regfile(io.write(i).writeAddr) := io.write(i).writeData
+    }
+  }
   io.debug := regfile
 }
 
-class RegFileIO extends Bundle {
-  val r1Addr = Input(UInt(regAddrWidth.W))
-  val r2Addr = Input(UInt(regAddrWidth.W))
-  val writeAddr = Input(UInt(regAddrWidth.W))
-  val writeData = Input(UInt(dataWidth.W))
-  val writeEnable = Input(Bool())
+class RegFileIO(numRead: Int, numWrite: Int) extends Bundle {
+  val read = Vec(numRead, new RegFileReadIO)
   ///
-  val r1Data = Output(UInt(dataWidth.W))
-  val r2Data = Output(UInt(dataWidth.W))
+  val write = Vec(numWrite, new RegFileWriteIO)
   ///
   val debug = Output(Vec(regAmount, UInt(dataWidth.W)))
+}
+
+class RegFileWriteIO extends Bundle {
+  val writeAddr   = Input(UInt(regAddrWidth.W))
+  val writeData   = Input(UInt(dataWidth.W))
+  val writeEnable = Input(Bool())
+}
+
+class RegFileReadIO extends Bundle {
+  val r1Addr = Input(UInt(regAddrWidth.W))
+  val r2Addr = Input(UInt(regAddrWidth.W))
+  val r1Data = Output(UInt(dataWidth.W))
+  val r2Data = Output(UInt(dataWidth.W))
 }
