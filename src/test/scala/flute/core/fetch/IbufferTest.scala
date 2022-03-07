be974f0d450a7b6c4dc7d27d81d566040895db07
Banson Xu <xzcxzcyy@126.com>
2022-03-07 11:28:57 +0800

    Fetch: ibuffer tested

--- src/test/scala/flute/core/fetch/IbufferTest.scala
+++ src/test/scala/flute/core/fetch/IbufferTest.scala
@@ -37,14 +37,23 @@ object IbufferFirrtlTester extends App {
   )
 
   val tester = TreadleTester(firrtlAnno)
-  val poke = tester.poke _
+  val peek   = tester.peek _
+  val poke   = tester.poke _
   var clock  = 0
+  def printRead(i: Int) = {
+    println(
+      s"io_read_${i}_bits = " +
+        tester.peek(s"io_read_${i}_bits") +
+        s"; io_read_${i}_valid = " +
+        tester.peek(s"io_read_${i}_valid")
+    )
+  }
   def step(n: Int = 1) = {
     clock += n
     tester.step(n)
+    println(s"================ total clock steped: ${clock} ================")
   }
   def printElements() = {
-    println(s"================ total clock steped: ${clock} ================")
     for (i <- 0 until 16) {
       println(s"data_${i}\t" + tester.peek(s"data_${i}"))
     }
@@ -66,9 +75,99 @@ object IbufferFirrtlTester extends App {
 
   printElements()
 
+  for (i <- 3 to 7) {
+    poke(s"io_write_${i}_bits", i * 10)
+    poke(s"io_write_${i}_valid", 1)
+  }
   step()
+  for (i <- 3 to 7) {
+    poke(s"io_write_${i}_bits", 0)
+    poke(s"io_write_${i}_valid", 0)
+  }
 
   printElements()
-  
+
+  for (i <- 2 to 6) {
+    poke(s"io_write_${i}_bits", i * 100)
+    poke(s"io_write_${i}_valid", 1)
+  }
+  for (i <- 0 to 1) {
+    poke(s"io_read_${i}_ready", 1)
+    printRead(i)
+  }
+  step()
+  for (i <- 2 to 6) {
+    poke(s"io_write_${i}_bits", 0)
+    poke(s"io_write_${i}_valid", 0)
+  }
+  for (i <- 0 to 1) {
+    poke(s"io_read_${i}_ready", 0)
+  }
+
+  printElements()
+
+  for (i <- 1 to 5) {
+    poke(s"io_write_${i}_bits", i * 1000)
+    poke(s"io_write_${i}_valid", 1)
+  }
+  step()
+  for (i <- 1 to 5) {
+    poke(s"io_write_${i}_bits", i * 1000)
+    poke(s"io_write_${i}_valid", 0)
+  }
+
+  printElements()
+
+  for (i <- 0 to 1) {
+    if (i == 0) {
+      poke(s"io_read_${i}_ready", 1)
+    } else {
+      poke(s"io_read_${i}_ready", 0)
+    }
+    printRead(i)
+  }
+  step()
+  for (i <- 0 to 1) {
+    poke(s"io_read_${i}_ready", 0)
+  }
+
+  for (j <- 0 until 10) {
+    for (i <- 0 to 1) {
+      poke(s"io_read_${i}_ready", 1)
+      printRead(i)
+    }
+    step()
+    for (i <- 0 to 1) {
+      poke(s"io_read_${i}_ready", 0)
+    }
+  }
+
+  printElements()
+
+  for (i <- 0 to 7) {
+    poke(s"io_write_${i}_bits", i * 10000)
+    poke(s"io_write_${i}_valid", 1)
+  }
+  step()
+  for (i <- 0 to 7) {
+    poke(s"io_write_${i}_bits", 0)
+    poke(s"io_write_${i}_valid", 0)
+  }
+
+  printElements()
+
+  for (j <- 0 until 10) {
+    for (i <- 0 to 1) {
+      poke(s"io_read_${i}_ready", 1)
+      printRead(i)
+    }
+    step()
+    for (i <- 0 to 1) {
+      poke(s"io_read_${i}_ready", 0)
+    }
+  }
+
+  printElements()
+
   tester.report()
 }
