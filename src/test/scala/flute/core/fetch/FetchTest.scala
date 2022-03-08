a8d1aed538a3ba79af965e5409bc1965b7a1c312
Banson Xu <xzcxzcyy@126.com>
2022-03-08 12:10:22 +0800

    Fetch: add test for beq bne testbench

--- src/test/scala/flute/core/fetch/FetchTest.scala
+++ src/test/scala/flute/core/fetch/FetchTest.scala
@@ -18,6 +18,8 @@ import chisel3.stage.ChiselStage
 
 class FetchTest extends AnyFreeSpec with Matchers with ChiselScalatestTester {
   "Fetch1: base" in {
+    println("=================== Fetch1: base ===================")
+
     val firrtlAnno = (new ChiselStage).execute(
       Array(),
       Seq(
@@ -85,6 +87,140 @@ class FetchTest extends AnyFreeSpec with Matchers with ChiselScalatestTester {
     displayReadPort()
 
     t.report()
+  }
+
+  "beq bne" in {
+    println("=================== beq bne ===================")
+
+    val firrtlAnno = (new ChiselStage).execute(
+      Array(),
+      Seq(
+        TargetDirAnnotation("target"),
+        ChiselGeneratorAnnotation(() => new FetchTestTop("beq_bne"))
+      )
+    )
+
+    val t     = TreadleTester(firrtlAnno)
+    val poke  = t.poke _
+    val peek  = t.peek _
+    var clock = 0
+    def step(n: Int = 1) = {
+      t.step(n)
+      clock += n
+      println(s">>>>>>>>>>>>>>>>>> Total clock steped: ${clock} ")
+    }
+    def displayReadPort() = {
+      for (i <- 0 until 2) {
+        val instruction = peek(s"io_withDecode_ibufferEntries_${i}_bits_inst")
+        val address     = peek(s"io_withDecode_ibufferEntries_${i}_bits_addr")
+        val valid       = peek(s"io_withDecode_ibufferEntries_${i}_valid")
+        println(s"inst #$i: ${"%08x".format(instruction)}")
+        println(s"addr #$i: ${"%08x".format(address)}")
+        println(s"valid #$i: ${valid}")
+      }
+      println("state: " + peek(s"fetch.state"))
+    }
+
+    step()
+    displayReadPort()
+
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 1)
+    }
+    step()
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 0)
+    }
+    displayReadPort()
+
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 1)
+    }
+    step()
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 0)
+    }
+    displayReadPort()
+
+    poke(s"io_feedbackFromExec_branchAddr_valid", 1)
+    poke(s"io_feedbackFromExec_branchAddr_bits", 0x14)
+    step(1)
+    poke(s"io_feedbackFromExec_branchAddr_valid", 0)
+    poke(s"io_feedbackFromExec_branchAddr_bits", 0)
+    displayReadPort()
+
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 1)
+    }
+    step()
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 0)
+    }
+    displayReadPort()
+
+    step()
+
+    displayReadPort()
+
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 1)
+    }
+    step()
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 0)
+    }
+    displayReadPort()
+
+    poke(s"io_feedbackFromExec_branchAddr_valid", 1)
+    poke(s"io_feedbackFromExec_branchAddr_bits", 0x28)
+    step(1)
+    poke(s"io_feedbackFromExec_branchAddr_valid", 0)
+    poke(s"io_feedbackFromExec_branchAddr_bits", 0)
+    displayReadPort()
+
+    step(2)
+
+    displayReadPort()
+    
+    poke(s"io_feedbackFromExec_branchAddr_valid", 1)
+    poke(s"io_feedbackFromExec_branchAddr_bits", 0x30)
+    step(1)
+    poke(s"io_feedbackFromExec_branchAddr_valid", 0)
+    poke(s"io_feedbackFromExec_branchAddr_bits", 0)
+    displayReadPort()
+
+    step(2)
+
+    displayReadPort()
+
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 1)
+    }
+    step()
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 0)
+    }
+    displayReadPort()
+
+    poke(s"io_feedbackFromExec_branchAddr_valid", 1)
+    poke(s"io_feedbackFromExec_branchAddr_bits", 0x44)
+    step(1)
+    poke(s"io_feedbackFromExec_branchAddr_valid", 0)
+    poke(s"io_feedbackFromExec_branchAddr_bits", 0)
+    displayReadPort()
+
+    step(2)
+
+    displayReadPort()
+
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 1)
+    }
+    step()
+    for (i <- 0 until 2) {
+      poke(s"io_withDecode_ibufferEntries_${i}_ready", 0)
+    }
+    displayReadPort()
   }
 }
 
