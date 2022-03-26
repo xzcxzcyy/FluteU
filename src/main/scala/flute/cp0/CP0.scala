9c96fdfddfca59abf955a79af73dbf7a9a9937eb
Banson Xu <xzcxzcyy@126.com>
2022-03-26 23:27:01 +0800

    cp0: add inside logic

--- src/main/scala/flute/cp0/CP0.scala
+++ src/main/scala/flute/cp0/CP0.scala
@@ -4,6 +4,7 @@ import chisel3._
 import chisel3.util._
 
 import flute.config.CPUConfig._
+import chisel3.stage.ChiselStage
 
 class CP0Read extends Bundle {
   val addr = Input(UInt(5.W))
@@ -20,7 +21,7 @@ class CP0Write extends Bundle {
 
 class CP0WithCommit extends Bundle {
   val pc     = Input(UInt(addrWidth.W))
-  val inSlot = Input(Bool())
+  val inSlot = Input(Bool()) // whether the instruction in pc is delay slot
 }
 
 class CP0 extends Module {
@@ -29,7 +30,7 @@ class CP0 extends Module {
     val write   = new CP0Write
     val commit  = new CP0WithCommit
     val hwIntr  = Input(UInt(6.W))
-    val intrReq = Output(Bool())
+    val intrReq = Output(Bool()) // 例外输出信号
   })
 
   val badvaddr = new CP0BadVAddr
@@ -37,4 +38,60 @@ class CP0 extends Module {
   val status   = new CP0Status
   val cause    = new CP0Cause
   val epc      = new CP0EPC
+  val countInc = RegInit(0.B)
+
+  def wReq(r: CP0BaseReg): Bool = {
+    io.write.enable && io.write.addr === r.addr.U && io.write.sel === r.sel.U
+  }
+
+  val regs = Seq(
+    badvaddr,
+    count,
+    status,
+    cause,
+    epc,
+  )
+  val readRes = WireInit(0.U(dataWidth.W))
+  regs.foreach( r =>
+    when(io.read.addr === r.addr.U && io.read.sel === r.sel.U) {
+      readRes := r.reg.asUInt
+    }
+  )
+  io.read.data := readRes
+
+  countInc := !countInc
+
+  // badvaddr
+
+  // count
+  when(wReq(count)) {
+    count.reg := io.write.data
+  }.otherwise {
+    count.reg := count.reg + countInc.asUInt
+  }
+
+  // status
+  val writeStatusWire = WireInit(io.write.data.asTypeOf(new StatusBundle))
+  when(wReq(status)) {
+    status.reg.exl := writeStatusWire.exl
+    status.reg.ie  := writeStatusWire.ie
+    status.reg.im  := writeStatusWire.im
+  }
+
+  // cause
+  val writeCauseWire = WireInit(io.write.data.asTypeOf(new CauseBundle))
+  when(wReq(cause)) {
+    for (i <- 0 to 1) yield {
+      cause.reg.ip(i) := writeCauseWire.ip(i)
+    }
+  }
+
+  // epc
+  when(wReq(epc)) {
+    epc.reg := io.write.data
+  }
+}
+
+object CP0Main extends App {
+  (new ChiselStage).emitVerilog(new CP0, Array("--target-dir", "target/verilog", "--target:fpga"))
 }
--- src/main/scala/flute/cp0/Regs.scala
+++ src/main/scala/flute/cp0/Regs.scala
@@ -10,13 +10,13 @@ abstract class CP0BaseReg {
 }
 
 class CP0BadVAddr extends CP0BaseReg {
-  override val reg: Data = RegInit(0.U(dataWidth.W))
+  override val reg = RegInit(0.U(dataWidth.W))
   override val addr: Int = 8
   override val sel: Int  = 0
 }
 
 class CP0Count extends CP0BaseReg {
-  override val reg: Data = RegInit(0.U(dataWidth.W))
+  override val reg = RegInit(0.U(dataWidth.W))
   override val addr: Int = 9
   override val sel: Int  = 0
 }
@@ -25,14 +25,14 @@ class StatusBundle extends Bundle {
   val zero_31_23 = UInt(9.W)
   val bev        = Bool()
   val zero_21_16 = UInt(6.W)
-  val im         = UInt(8.W)
+  val im         = Vec(8, Bool())
   val zero_7_2   = UInt(6.W)
   val exl        = Bool()
   val ie         = Bool()
 }
 
 class CP0Status extends CP0BaseReg {
-  override val reg: Data = RegInit({
+  override val reg = RegInit({
     val bundle = WireInit(0.U.asTypeOf(new StatusBundle))
     bundle.bev := 1.B
     bundle
@@ -46,21 +46,21 @@ class CauseBundle extends Bundle {
   val bd         = Bool()
   val ti         = Bool()
   val zero_29_16 = UInt(14.W)
-  val ip         = UInt(8.W)
+  val ip         = Vec(8, Bool())
   val zero_7     = Bool()
   val excCode    = UInt(5.W)
   val zero_1_0   = UInt(2.W)
 }
 
 class CP0Cause extends CP0BaseReg {
-  override val reg: Data = RegInit(0.U.asTypeOf(new CauseBundle))
+  override val reg: CauseBundle = RegInit(0.U.asTypeOf(new CauseBundle))
   assert(reg.getWidth == dataWidth)
   override val addr: Int = 13
   override val sel: Int  = 0
 }
 
 class CP0EPC extends CP0BaseReg {
-  override val reg: Data = RegInit(0.U(dataWidth.W))
+  override val reg = RegInit(0.U(dataWidth.W))
   override val addr: Int = 14
   override val sel: Int  = 0
 }
