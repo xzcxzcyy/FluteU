96fb5b261b0201d426a2058b0c5f1c1c6b321ee7
Chengyuan <1136427613@qq.com>
2022-06-10 17:21:40 +0800

    add: Rename Mapping Table sRAT and aRAT

---
+++ src/main/scala/flute/core/rename/RMT.scala
@@ -0,0 +1,81 @@
+package flute.core.rename
+
+import chisel3._
+import chisel3.util._
+import flute.config.CPUConfig._
+
+class RMTReadPort extends Bundle {
+  val addr = Input(UInt(archRegAddrWidth.W))
+  val data = Output(UInt(phyRegAddrWidth.W))
+}
+
+class RMTWritePort extends Bundle {
+  val en   = Input(Bool())
+  val addr = Input(UInt(archRegAddrWidth.W))
+  val data = Input(UInt(phyRegAddrWidth.W))
+}
+
+// Map: arch -> phy
+class RMT(numWays: Int, numCommit: Int, release: Boolean = false) extends Module {
+  val io = IO(new Bundle {
+    // 每条指令需要三个读端口 0->dest;1->src1;2->src2
+    val read = Vec(numWays, Vec(3, new RMTReadPort))
+
+    // 每条指令需要一个写端口
+    val write = Vec(numWays, new RMTWritePort)
+
+    // commit to aRAT
+    val commit = new Bundle {
+      val write = Vec(numCommit, new RMTWritePort)
+    }
+
+    val chToArch = Input(Bool())
+
+    val debug = if (!release) Some(new Bundle {
+      val sRAT = Output(Vec(archRegAmount, UInt(phyRegAddrWidth.W)))
+      val aRAT = Output(Vec(archRegAmount, UInt(phyRegAddrWidth.W)))
+    })
+    else None
+  })
+
+  // reset init all arch reg map to phy reg $0
+  val sRAT = RegInit(VecInit(Seq.fill(archRegAmount)(0.U(phyRegAddrWidth.W))))
+  val aRAT = RegInit(VecInit(Seq.fill(archRegAmount)(0.U(phyRegAddrWidth.W))))
+
+  // sRAT
+  for (i <- 0 until numWays; tp <- 0 until 3) {
+    val archRegAddr = io.read(i)(tp).addr
+    val phyRegAddr  = sRAT(archRegAddr)
+    io.read(i)(tp).data := phyRegAddr
+  }
+
+  for (i <- 0 until numWays) {
+    val en          = io.write(i).en
+    val archRegAddr = io.write(i).addr
+    val phyRegAddr  = io.write(i).data
+    when(en && !io.chToArch) {
+      sRAT(archRegAddr) := phyRegAddr
+    }
+  }
+
+  // aRAT
+  for (i <- 0 until numCommit) {
+    val en          = io.commit.write(i).en
+    val archRegAddr = io.commit.write(i).addr
+    val phyRegAddr  = io.commit.write(i).data
+    when(en && !io.chToArch) {
+      sRAT(archRegAddr) := phyRegAddr
+    }
+  }
+
+  // aRAT -> sRAT
+  when(io.chToArch) {
+    sRAT := aRAT
+  }
+
+  if (!release) {
+    io.debug.get.aRAT := aRAT
+    io.debug.get.sRAT := sRAT
+  }
+
+}
