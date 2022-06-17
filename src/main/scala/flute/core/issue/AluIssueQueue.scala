29f8b2b746e2e993bb2ca0bbfc0bc7ca49f79114
Banson Xu <xzcxzcyy@126.com>
2022-06-17 17:02:54 +0800

    feat: AluIssueQueue entryMove.

--- src/main/scala/flute/core/issue/AluIssueQueue.scala
+++ src/main/scala/flute/core/issue/AluIssueQueue.scala
@@ -6,13 +6,13 @@ import flute.config.CPUConfig._
 import flute.core.decode.MicroOp
 
 class AluIqEntry extends Bundle {
-  val uop = new MicroOp
+  val uop    = new MicroOp
   val awaken = Bool()
 }
 
 class AluIssueQueue(volume: Int, detectWidth: Int) extends Module {
   private val enqNum = 2
-  
+
   private val deqNum = 2
 
   val io = IO(new Bundle {
@@ -26,14 +26,47 @@ class AluIssueQueue(volume: Int, detectWidth: Int) extends Module {
 
 }
 
+object AluIssueQueueComponents {
+  def entryMove(issued: Seq[UInt], issueCount: UInt, volume: Int) = {
+    assert(issued.length == 2)
+    val n        = issued.length
+    val movement = Seq.fill(volume)(WireInit(0.U(log2Up(volume).W)))
+    when(issueCount === 1.U) {
+      for (i <- 0 until volume) yield {
+        movement(i) := Mux(i.U >= issued(0), 1.U, 0.U)
+      }
+    }.elsewhen(issueCount === 2.U) {
+      for (i <- 0 until volume) yield {
+        when((i + 1).U >= issued(1)) {
+          movement(i) := 2.U
+        }.elsewhen(i.U >= issued(0)) {
+          movement(i) := 1.U
+        }.otherwise {
+          movement(i) := 0.U
+        }
+      }
+    }
+    movement
+  }
+}
+
 // 1 2 3 4 5 6 7
 //     3   5 - out
 // 1 2 4 6 7
-// 
+//
 // 1 2 3 4 5 6 7 8
 //     3     6 - out
 // 1 2 4 5 7 8
 // 1 2 3 4 5 6 7 8 9 a b c
 //   2     5     8 - out
-// 1 3 4 6 7 9 a b c 
+// 1 3 4 6 7 9 a b c
+// 0 1 1 2 2 3 3 3 3
 
+// 1 2 3 4 5 6 7 8 9 a b c
+//   2     5 6 - out
+// 1 3 4 7 8 9 a b c
+// 0 1 1 3 3 3 3 3 3
+// 0 0 0 0 0 0 0 0 0
+// 0 1 1 1 1 1 1 1 1
+// 0 0 0 1 1 1 1 1 1
+// 0 0 0 1 1 1 1 1 1
