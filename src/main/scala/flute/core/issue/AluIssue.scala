d52c619e2e3841a97e72a2274a3738ed303207c3
Chengyuan <1136427613@qq.com>
2022-07-11 20:14:24 +0800

    add: init AluIssue

--- src/main/scala/flute/core/issue/AluIssue.scala
+++ src/main/scala/flute/core/issue/AluIssue.scala
@@ -3,13 +3,24 @@ package flute.core.issue
 import chisel3._
 import chisel3.util._
 import flute.core.decode.MicroOp
+import flute.core.rename.BusyTableReadPort
 
-class AluIssue extends Module {
+class AluEntry extends Bundle {
+  private val deqNum = 2
+  val uop            = new MicroOp(rename = true)
+  val awaken         = Bool()
+  val sel            = UInt(log2Ceil(deqNum).W)
+}
+class AluIssue(detectWidth: Int) extends Module {
   val io = IO(new Bundle {
-
     // 接收窗口
-    val win = Input(Vec(4, Valid(new MicroOp(rename = true))))
-    
+    val detect = Input(Vec(detectWidth, Valid(new MicroOp(rename = true))))
+    // data from Issue Stage Reg
+    val wake = Input(Vec(2, Valid(new AluEntry)))
 
+    val bt = Vec(2 * detectWidth, Flipped(new BusyTableReadPort))
+
+    val issue = Output(Vec(2, Valid(UInt(detectWidth.W))))
+    val out   = Output(Vec(2, Valid(new AluEntry)))
   })
 }
