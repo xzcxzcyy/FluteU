c4417db50c41c7ceae751d411e40f3d84cbcb650
Chengyuan <1136427613@qq.com>
2022-07-16 23:06:12 +0800

    chore: TODO

--- src/main/scala/flute/core/rob/Commit.scala
+++ src/main/scala/flute/core/rob/Commit.scala
@@ -5,15 +5,25 @@ import chisel3.util._
 import flute.core.decode.StoreMode
 import flute.core.rename.RenameCommit
 import flute.config.CPUConfig._
+import flute.cp0.CP0WithCommit
+import flute.cache.top.DCacheReq
 
 class StoreCommit extends Bundle {}
-class BranchCommit extends Bundle {
-  val valid = Bool() // 全局有效位
-  val pc = UInt(addrWidth.W) // 指令pc
-  val taken = Bool() // 是否跳转
+
+class BranchTrain extends Bundle {
+  val pc     = UInt(addrWidth.W) // 指令pc
+  val taken  = Bool()            // 是否跳转
   val target = UInt(instrWidth.W)
 }
-class Commit(nCommit: Int) extends Module {
+
+class BranchCommit extends Bundle {
+  val train     = Valid(new BranchTrain)
+  val pcRestore = Valid(UInt(addrWidth.W))
+}
+
+class Commit(nCommit: Int = 2) extends Module {
+  assert(nCommit == 2)
+
   val io = IO(new Bundle {
     val rob = Vec(nCommit, Flipped(Decoupled(new ROBEntry)))
 
@@ -22,6 +32,11 @@ class Commit(nCommit: Int) extends Module {
     val store  = Output(new StoreCommit)
     val branch = Output(new BranchCommit)
 
+    val cp0     = Flipped(new CP0WithCommit)
+    val intrReq = Input(Bool())
+
+    val dCache = DecoupledIO(new DCacheReq)
+
     val recover = Output(Bool())
   })
 
@@ -29,26 +44,19 @@ class Commit(nCommit: Int) extends Module {
 
   val validMask = WireDefault(VecInit(io.rob.map(r => r.valid)))
 
+  val isStore = WireInit(VecInit(robRaw.map(r => r.memWMode =/= StoreMode.disable)))
+
   val completeMask = Wire(Vec(nCommit, Bool()))
+
   for (i <- 0 until nCommit) {
-    var complete = robRaw(i).complete
+    var complete = robRaw(i).complete && (!isStore(i) || io.dCache.ready)
     for (j <- 0 until i) {
-      complete = complete && robRaw(j).complete
+      complete = complete && robRaw(j).complete && (!isStore(i) || io.dCache.ready)
     }
     completeMask(i) := complete
   }
 
-  val branchMask = Wire(Vec(nCommit, Bool()))
-  for (i <- 0 until nCommit) {
-    var hasNoBranchBefore = 1.B
-    for (j <- 0 until i) {
-      hasNoBranchBefore = hasNoBranchBefore && !robRaw(i).branch
-    }
-    branchMask(i) := !robRaw(i).branch || hasNoBranchBefore
-  }
-
   val storeMask = Wire(Vec(nCommit, Bool()))
-  val isStore   = WireInit(VecInit(robRaw.map(r => r.memWMode =/= StoreMode.disable)))
 
   for (i <- 0 until nCommit) {
     var hasNoStoreBefore = 1.B
@@ -58,64 +66,94 @@ class Commit(nCommit: Int) extends Module {
     storeMask(i) := !isStore(i) || hasNoStoreBefore
   }
 
+  val cp0Mask = WireInit(VecInit(Seq.fill(nCommit)(!io.intrReq)))
+
   val programException = Wire(Vec(nCommit, Bool()))
-  val branchException  = Wire(Vec(nCommit, Bool()))
+  val branchFail       = Wire(Vec(nCommit, Bool()))
 
   for (i <- 0 until nCommit) {
     programException(i) := robRaw(i).exception.asUInt.orR
-    branchException(i)  := robRaw(i).branch && robRaw(i).predictBT =/= robRaw(i).computeBT
+    branchFail(i)       := robRaw(i).branch && robRaw(i).predictBT =/= robRaw(i).computeBT
   }
-  val hasException = (0 until nCommit).map(i => programException(i) || branchException(i))
+  val hasExceptionOrFail = Wire(Vec(2, Bool()))
 
-  val exceptionMask = Wire(Vec(nCommit, Bool()))
-  exceptionMask(0) := 1.B
-  for (i <- 1 until nCommit) {
-    var hasNoExcptBefore = 1.B
-    for (j <- 0 to i) { // include itself
-      hasNoExcptBefore = hasNoExcptBefore && !hasException(j)
-    }
-    exceptionMask(i) := hasNoExcptBefore
-  }
+  hasExceptionOrFail(0) := 0.B
+  hasExceptionOrFail(1) := programException(1) || branchFail(1)
 
-  val finalMask = WireDefault(
+  val mask1 = WireDefault(
     VecInit(
       (0 until nCommit).map(i =>
-        validMask(i) && completeMask(i) && branchMask(i) && storeMask(i) && exceptionMask(i)
+        validMask(i) && completeMask(i) && storeMask(i) && !hasExceptionOrFail(i) && cp0Mask(i)
       )
     )
   )
 
-  // Rename Commit
-  for (i <- 0 until nCommit) {
-    // [[io.commit]]
-    // 数据通路
-    io.commit.rmt.write(i).addr      := robRaw(i).logicReg
-    io.commit.rmt.write(i).data      := robRaw(i).physicReg
-    io.commit.freelist.free(i).bits  := robRaw(i).originReg
-    io.commit.freelist.alloc(i).bits := robRaw(i).physicReg
-    // 控制信号
-    val wbValid = finalMask(i) && !hasException(i) && robRaw(i).regWEn
-    io.commit.rmt.write(i).en         := wbValid
-    io.commit.freelist.free(i).valid  := wbValid
-    io.commit.freelist.alloc(i).valid := wbValid
+  val cycleEn = WireInit(1.B)
 
-    // TODO may be only work for ALU INSTR
-    io.rob(i).ready := robRaw(i).regWEn && completeMask(i)
-  }
-  io.commit.chToArch := finalMask(0) && hasException(0)
-  io.recover         := finalMask(0) && hasException(0)
-
-  for (i <- 0 until nCommit) {
-    when(finalMask(i) && robRaw(i).branch) {
-      // commit to branch predictor
-    }
-
-    when(finalMask(i) && isStore(i)) {
-      // commit to LSU & DCache
-    }
+  when(branchFail(0) && !completeMask(1)) {
+    cycleEn := 0.B
   }
 
-  io.branch := DontCare
-  io.store  := DontCare
+  val wbEn = WireInit(1.B)
+
+
+
+
+  // val exceptionMask = Wire(Vec(nCommit, Bool()))
+  // exceptionMask(0) := 1.B
+  // for (i <- 1 until nCommit) {
+  //   var hasNoExcptBefore = 1.B
+  //   for (j <- 0 to i) { // include itself
+  //     hasNoExcptBefore = hasNoExcptBefore && !hasException(j)
+  //   }
+  //   exceptionMask(i) := hasNoExcptBefore
+  // }
+
+  // val finalMask = WireDefault(
+  //   VecInit(
+  //     (0 until nCommit).map(i =>
+  //       validMask(i) && completeMask(i) && storeMask(i) && exceptionMask(i)
+  //     )
+  //   )
+  // )
+
+  // // Rename Commit
+  // for (i <- 0 until nCommit) {
+  //   // [[io.commit]]
+  //   // 数据通路
+  //   io.commit.rmt.write(i).addr      := robRaw(i).logicReg
+  //   io.commit.rmt.write(i).data      := robRaw(i).physicReg
+  //   io.commit.freelist.free(i).bits  := robRaw(i).originReg
+  //   io.commit.freelist.alloc(i).bits := robRaw(i).physicReg
+  //   // 控制信号
+  //   val wbValid = finalMask(i) && !hasException(i) && robRaw(i).regWEn
+  //   io.commit.rmt.write(i).en         := wbValid
+  //   io.commit.freelist.free(i).valid  := wbValid
+  //   io.commit.freelist.alloc(i).valid := wbValid
+
+  //   // TODO may be only work for ALU INSTR
+  //   io.rob(i).ready := robRaw(i).regWEn && completeMask(i)
+  // }
+  // io.commit.chToArch := finalMask(0) && hasException(0)
+  // io.recover         := finalMask(0) && hasException(0)
+
+  // val branchCommit = WireInit(0.U.asTypeOf(new BranchCommit))
+
+  // for (i <- 0 until nCommit) {
+  //   when(finalMask(i) && robRaw(i).branch) {
+  //     // commit to branch predictor
+  //     branchCommit.valid  := 1.B
+  //     branchCommit.pc     := robRaw(i).pc
+  //     branchCommit.taken  := robRaw(i).branchTaken
+  //     branchCommit.target := robRaw(i).computeBT
+  //   }
+
+  //   when(finalMask(i) && isStore(i)) {
+  //     // commit to LSU & DCache
+  //   }
+  // }
+
+  // io.branch := branchCommit
+  // io.store  := DontCare
 
 }
