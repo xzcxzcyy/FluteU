3889513d5f0d55dc6f58c6beac4ea005d6922653
Banson Xu <xzcxzcyy@126.com>
2022-07-18 11:59:24 +0800

    feat: Working on Commit logic

--- src/main/scala/flute/core/rob/Commit.scala
+++ src/main/scala/flute/core/rob/Commit.scala
@@ -8,7 +8,10 @@ import flute.config.CPUConfig._
 import flute.cp0.CP0WithCommit
 import flute.cache.top.DCacheReq
 
-class StoreCommit extends Bundle {}
+class StoreCommit extends Bundle {
+  val req    = DecoupledIO(new DCacheReq)
+  val hazard = Output(Bool())
+}
 
 class BranchTrain extends Bundle {
   val pc     = UInt(addrWidth.W) // 指令pc
@@ -25,18 +28,13 @@ class Commit(nCommit: Int = 2) extends Module {
   assert(nCommit == 2)
 
   val io = IO(new Bundle {
-    val rob = Vec(nCommit, Flipped(Decoupled(new ROBEntry)))
-
-    val commit = Flipped(new RenameCommit(nCommit))
-
-    val store  = Output(new StoreCommit)
-    val branch = Output(new BranchCommit)
-
-    val cp0     = Flipped(new CP0WithCommit)
+    val rob     = Vec(nCommit, Flipped(Decoupled(new ROBEntry)))
     val intrReq = Input(Bool())
 
-    val dCache = DecoupledIO(new DCacheReq)
-
+    val commit  = Flipped(new RenameCommit(nCommit))
+    val branch  = Output(new BranchCommit)
+    val store   = new StoreCommit
+    val cp0     = Flipped(new CP0WithCommit)
     val recover = Output(Bool())
   })
 
@@ -49,9 +47,9 @@ class Commit(nCommit: Int = 2) extends Module {
   val completeMask = Wire(Vec(nCommit, Bool()))
 
   for (i <- 0 until nCommit) {
-    var complete = robRaw(i).complete && (!isStore(i) || io.dCache.ready)
+    var complete = robRaw(i).complete && (!isStore(i) || io.store.req.ready)
     for (j <- 0 until i) {
-      complete = complete && robRaw(j).complete && (!isStore(i) || io.dCache.ready)
+      complete = complete && robRaw(j).complete && (!isStore(i) || io.store.req.ready)
     }
     completeMask(i) := complete
   }
@@ -75,27 +73,28 @@ class Commit(nCommit: Int = 2) extends Module {
 
   for (i <- 0 until nCommit) {
     programException(i) := existMask(i) && robRaw(i).exception.asUInt.orR
-    branchFail(i)       := existMask(i) && robRaw(i).branch && robRaw(i).predictBT =/= robRaw(i).computeBT
+    branchFail(i) := existMask(i) && robRaw(i).branch && robRaw(i).predictBT =/= robRaw(i).computeBT
   }
-  
+
   val restMask = WireInit(VecInit(Seq.fill(nCommit)(1.B)))
-  when (branchFail(0) && !existMask(1)) {
+  when(branchFail(0) && !existMask(1)) {
     restMask(0) := 0.B
   }
-  when (programException(1)) {
+  when(programException(1)) {
     restMask(1) := 0.B
   }
-  when (branchFail(1)) {
+  when(branchFail(1)) {
     restMask(1) := 0.B
   }
 
-  val finalMask = WireInit(VecInit(
-    for (i <- 0 until nCommit) yield {
-      existMask(i) && storeMask(i) && cp0Mask(i) && restMask(i)
-    }
-  ))
+  val finalMask = WireInit(
+    VecInit(
+      for (i <- 0 until nCommit) yield {
+        existMask(i) && storeMask(i) && cp0Mask(i) && restMask(i)
+      }
+    )
+  )
 
-  
   // [[io.commit]] 数据通路
   for (i <- 0 until nCommit) {
     io.commit.rmt.write(i).addr      := robRaw(i).logicReg
@@ -111,57 +110,45 @@ class Commit(nCommit: Int = 2) extends Module {
   val branchRecovery = branchFail(0) && finalMask(1)
 
   io.commit.chToArch := branchRecovery || io.intrReq
-  io.recover := branchRecovery
+  io.recover         := branchRecovery
 
   val branchTrain = WireInit(0.U.asTypeOf(Valid(new BranchTrain)))
   for (i <- 0 until nCommit) yield {
     // TODO: 分支训练时机可以选择
     when(branchFail(i)) {
-      branchTrain.valid := 1.B
-      branchTrain.bits.pc := robRaw(i).pc
-      branchTrain.bits.taken := robRaw(i).branchTaken
+      branchTrain.valid       := 1.B
+      branchTrain.bits.pc     := robRaw(i).pc
+      branchTrain.bits.taken  := robRaw(i).branchTaken
       branchTrain.bits.target := robRaw(i).computeBT
     }
   }
   io.branch.train := branchTrain
 
-  // // Rename Commit
-  // for (i <- 0 until nCommit) {
-  //   // [[io.commit]]
-  //   // 数据通路
-  //   io.commit.rmt.write(i).addr      := robRaw(i).logicReg
-  //   io.commit.rmt.write(i).data      := robRaw(i).physicReg
-  //   io.commit.freelist.free(i).bits  := robRaw(i).originReg
-  //   io.commit.freelist.alloc(i).bits := robRaw(i).physicReg
-  //   // 控制信号
-  //   val wbValid = finalMask(i) && !hasException(i) && robRaw(i).regWEn
-  //   io.commit.rmt.write(i).en         := wbValid
-  //   io.commit.freelist.free(i).valid  := wbValid
-  //   io.commit.freelist.alloc(i).valid := wbValid
+  val pcRestore = WireInit(0.U.asTypeOf(Valid(UInt(addrWidth.W))))
+  when(finalMask(0) && finalMask(1) && branchFail(0)) {
+    pcRestore.valid := 1.B
+    pcRestore.bits  := robRaw(0).computeBT
+  }
+  io.branch.pcRestore := pcRestore
 
-  //   // TODO may be only work for ALU INSTR
-  //   io.rob(i).ready := robRaw(i).regWEn && completeMask(i)
-  // }
-  // io.commit.chToArch := finalMask(0) && hasException(0)
-  // io.recover         := finalMask(0) && hasException(0)
+  val cacheReq = WireInit(0.U.asTypeOf(Valid(new DCacheReq)))
+  for (i <- 0 until nCommit) yield {
+    when(finalMask(i) && isStore(i)) {
+      cacheReq.valid          := 1.B
+      cacheReq.bits.addr      := robRaw(i).memWAddr
+      cacheReq.bits.storeMode := robRaw(i).memWMode
+      cacheReq.bits.writeData := robRaw(i).memWData
+    }
+  }
+  io.store.hazard    := cacheReq.valid
+  io.store.req.valid := cacheReq.valid
+  io.store.req.bits  := cacheReq.bits
 
-  // val branchCommit = WireInit(0.U.asTypeOf(new BranchCommit))
-
-  // for (i <- 0 until nCommit) {
-  //   when(finalMask(i) && robRaw(i).branch) {
-  //     // commit to branch predictor
-  //     branchCommit.valid  := 1.B
-  //     branchCommit.pc     := robRaw(i).pc
-  //     branchCommit.taken  := robRaw(i).branchTaken
-  //     branchCommit.target := robRaw(i).computeBT
-  //   }
-
-  //   when(finalMask(i) && isStore(i)) {
-  //     // commit to LSU & DCache
-  //   }
-  // }
-
-  // io.branch := branchCommit
-  // io.store  := DontCare
+  io.cp0.completed  := validMask(0) && robRaw(0).complete
+  io.cp0.exceptions := robRaw(0).exception
+  // TODO: support ERET; notify inSlot(IMPORTANT) 
+  io.cp0.eret   := 0.B
+  io.cp0.inSlot := 0.B
+  io.cp0.pc     := robRaw(0).pc
 
 }
